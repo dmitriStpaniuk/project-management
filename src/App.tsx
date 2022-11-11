3eb3d38626a60cf61a64726f990bc828a1ccdb5d
ivnpotapov <iojikov@mail.ru>
2022-11-11 21:10:20 +0700

    feat: add axios for users

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from 'store/store';
-import { getUserThunk } from 'store/thunks/userThunk';
+import { AxiosTest } from 'tempTest/AxiosTest/AxiosTest';
 import './App.css';
 
 function App() {
-  const dispatch = useAppDispatch();
-  const user = useAppSelector((state) => state.user.user);
-  useEffect(() => {
-    dispatch(getUserThunk('1'));
-  }, []);
-  return <div>{JSON.stringify(user)}</div>;
+  return <AxiosTest />;
 }
 
 export default App;
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,11 +1,53 @@
 import axios from 'axios';
-import { User } from 'store/slices/userSlice';
+import { EApiParametrs, getTokenLocalStorage, EApiRoutes } from './apiConstants';
+import { TUserResponse, TSignupUserData, TLoginUserData, ITokenResponse } from './userServiceTypes';
 
-export const getUser = async (userId: string) => {
-  const result = await axios.get<User>('localhost:4000' + userId);
-  // Заглушка пока юзер не авторизован
-  // const result = (await new Promise((res) => {
-  //   setTimeout(() => res({ data: { id: '1', name: 'Bob' } }), 1000);
-  // })) as { data: User };
+const instanceUserAxios = axios.create({
+  baseURL: EApiParametrs.baseUrl,
+  headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+});
+
+instanceUserAxios.interceptors.request.use(
+  (config) => {
+    const token = getTokenLocalStorage();
+    if (config.headers && token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export const getAllUsers = async (): Promise<TUserResponse[]> => {
+  const result = await instanceUserAxios.get(EApiRoutes.users);
+  return result.data;
+};
+
+export const getUserById = async (userId: string): Promise<TUserResponse> => {
+  const result = await instanceUserAxios.get(`${EApiRoutes.users}/${userId}`);
+  return result.data;
+};
+
+export const deleteUserById = async (userId: string): Promise<void> => {
+  await instanceUserAxios.delete(`${EApiRoutes.users}/${userId}`);
+};
+
+export const updateUser = async (
+  userId: string,
+  userData: TSignupUserData
+): Promise<TUserResponse> => {
+  const result = await instanceUserAxios.put(`${EApiRoutes.users}/${userId}`, userData);
+  return result.data;
+};
+
+export const createNewUser = async (userData: TSignupUserData): Promise<TUserResponse> => {
+  const result = await instanceUserAxios.post(EApiRoutes.signup, userData);
+  return result.data;
+};
+
+export const signin = async (userData: TLoginUserData): Promise<ITokenResponse> => {
+  const result = await instanceUserAxios.post(EApiRoutes.signin, userData);
   return result.data;
 };
