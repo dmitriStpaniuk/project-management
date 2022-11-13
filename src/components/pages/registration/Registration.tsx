70e848669b01545feebf7ce244dad784f4a0f314
Vladimir <vladimirkukolovich@gmail.com>
2022-11-13 23:32:43 +0300

    fix: corrected for scss

--- src/components/pages/registration/Registration.tsx
+++ src/components/pages/registration/Registration.tsx
@@ -5,6 +5,7 @@ import { FormProvider, useForm } from 'react-hook-form';
 import { SignupUserData } from 'services/userServiceTypes';
 import { useAppDispatch } from 'store/store';
 import { createNewUserThunk } from 'store/thunks/userThunk';
+import styles from './Registration.module.scss';
 
 export default function Registration() {
   const dispatch = useAppDispatch();
@@ -19,18 +20,20 @@ export default function Registration() {
   };
 
   return (
-    <Paper style={{ marginTop: '65px', display: 'grid', gridRowGap: '20px', padding: '20px' }}>
-      <FormProvider {...methods}>
-        <FormInputText name="name" label="name" type="text" />
-        <FormInputText name="login" label="login" type="text" />
-        <FormInputText name="password" label="password" type="password" />
-      </FormProvider>
-      <Button onClick={handleSubmit(onSubmit)} variant={'contained'} disabled={isSubmitting}>
-        Submit
-      </Button>
-      <Button onClick={() => reset()} variant={'outlined'}>
-        Reset
-      </Button>
-    </Paper>
+    <div className="container">
+      <Paper style={{ marginTop: '65px', display: 'grid', gridRowGap: '20px', padding: '20px' }}>
+        <FormProvider {...methods}>
+          <FormInputText name="name" label="name" type="text" />
+          <FormInputText name="login" label="login" type="text" />
+          <FormInputText name="password" label="password" type="password" />
+        </FormProvider>
+        <Button onClick={handleSubmit(onSubmit)} variant={'contained'} disabled={isSubmitting}>
+          Submit
+        </Button>
+        <Button onClick={() => reset()} variant={'outlined'}>
+          Reset
+        </Button>
+      </Paper>
+    </div>
   );
 }
