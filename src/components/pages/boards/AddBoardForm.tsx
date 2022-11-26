318845e6a0a99a1e7b314548d7367dd3d1a4874a
Dmitry.Stepaniuk <polevaiabrest@gmail.com>
2022-11-26 12:42:17 +0300

    add: boards list

---
+++ src/components/pages/boards/AddBoardForm.tsx
@@ -0,0 +1,92 @@
+import { Button, FormControl, Paper, Typography } from '@mui/material';
+import { FormBoardInputText } from 'components/FormBoardInputText';
+import { useTranslate } from 'components/languageContext/languageContext';
+import { useAlerts } from 'components/SnackbarPanel';
+import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { createNewBoard } from 'services/boardService';
+import { CreateBoardData } from 'services/boardServiceTypes';
+import { useAppDispatch } from 'store/store';
+import { createNewBoardThunk } from 'store/thunks/boardThunk';
+import styles from './../login/Login.module.scss';
+
+type FormProps = {
+  setPopupFormAddBoard: (x: boolean) => void;
+};
+
+export const AddBoardForm = ({ setPopupFormAddBoard }: FormProps) => {
+  const addAlert = useAlerts();
+  const dispatch = useAppDispatch();
+  const successCreateBoard = useTranslate('alerts.successCreateBoard');
+  const errorCreateBoard = useTranslate('alerts.errorCreateBoard');
+  const nameBoard = useTranslate('form.boardName');
+  const descriptionBoard = useTranslate('form.boardDescriptoon');
+  const submitBoardRequest = useTranslate('buttons.submit');
+  const closeBoardCreateForm = useTranslate('buttons.close');
+  const titleBoardCreateForm = useTranslate('form.titleCreateBoard');
+
+  const methods = useForm({
+    defaultValues: { title: '', description: '' },
+    mode: 'onChange',
+  });
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const onSubmit = async (data: CreateBoardData) => {
+    console.log(data);
+    try {
+      await dispatch(createNewBoardThunk(data));
+      addAlert({ type: 'success', message: successCreateBoard });
+      setPopupFormAddBoard(false);
+      // navigate('/boards');
+    } catch {
+      addAlert({ type: 'error', message: errorCreateBoard });
+    }
+  };
+
+  return (
+    // <div className={styles.wrapper}>
+    <Paper
+      style={{
+        width: '50%',
+        padding: '20px',
+        display: 'grid',
+        gridRowGap: '20px',
+        justifyItems: 'center',
+        position: 'absolute',
+      }}
+    >
+      <Typography variant="h4" component="h4">
+        {titleBoardCreateForm}
+      </Typography>
+      <FormProvider {...methods}>
+        <FormBoardInputText name="title" label={nameBoard} type="text" />
+        <FormBoardInputText name="description" label={descriptionBoard} type="text" />
+      </FormProvider>
+
+      <Button
+        onClick={handleSubmit(onSubmit)}
+        className={styles.formButton}
+        color="info"
+        variant={'contained'}
+        disabled={isSubmitting}
+      >
+        {submitBoardRequest}
+      </Button>
+      <Button
+        className={styles.formButton}
+        onClick={() => {
+          setPopupFormAddBoard(false);
+        }}
+        variant={'outlined'}
+        color="error"
+      >
+        {closeBoardCreateForm}
+      </Button>
+    </Paper>
+    // </div>
+  );
+};
--- src/components/pages/boards/Boards.tsx
+++ src/components/pages/boards/Boards.tsx
@@ -1,5 +1,59 @@
-import React from 'react';
+import { Button, Container, Typography } from '@mui/material';
+import { useTranslate } from 'components/languageContext/languageContext';
+import React, { useEffect, useState } from 'react';
+import { AiOutlineCheck } from 'react-icons/ai';
+import { useAppDispatch, useAppSelector } from 'store/store';
+import { getAllUBoardsList } from 'store/thunks/boardThunk';
+import { AddBoardForm } from './AddBoardForm';
+import { BoardCard } from './BoardCard';
 
 export default function Board() {
-  return <div style={{ minHeight: 'inherit' }}>Boards</div>;
+  const [popupFormAddBoard, setPopupFormAddBoard] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+  const allBoardsList = useAppSelector((state) => state.board.allBoardsList);
+  const addBoardText = useTranslate('buttons.neweBoard');
+
+  const handleClick = () => {
+    setPopupFormAddBoard(true);
+  };
+
+  const allBoards = async () => {
+    await dispatch(getAllUBoardsList());
+  };
+
+  useEffect(() => {
+    allBoards();
+  }, [handleClick]);
+  return (
+    <div style={{ minHeight: 'inherit', backgroundColor: '#f6f6f6' }}>
+      <Container
+        data-testid="boards"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: 'inherit',
+          justifyContent: ' space-between',
+          alignItems: 'center',
+          paddingTop: '10px',
+          paddingBottom: '20px',
+        }}
+      >
+        <Typography variant="h2" component="h3" sx={{ textAlign: 'center' }}>
+          Boards
+        </Typography>
+        {allBoardsList?.map((board) => (
+          <BoardCard key={board.id} title={board.title} description={board.description} />
+        ))}
+        {popupFormAddBoard ? <AddBoardForm setPopupFormAddBoard={setPopupFormAddBoard} /> : null}
+        <Button
+          sx={{ width: '30%', fontSize: 12 }}
+          variant="outlined"
+          startIcon={<AiOutlineCheck />}
+          onClick={handleClick}
+        >
+          {addBoardText}
+        </Button>
+      </Container>
+    </div>
+  );
 }
